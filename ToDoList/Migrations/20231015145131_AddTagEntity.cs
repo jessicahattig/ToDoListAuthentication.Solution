4218d1856d78a96e7ef005a76e9bc4c9996b5711
jessicahattig <jessicahattig@gmail.com>
2023-10-22 10:17:12 -0700

    Create project with clone.

---
+++ ToDoList/Migrations/20231015145131_AddTagEntity.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class AddTagEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.TagId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CategoryId",
+                table: "Items",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Items");
+        }
+    }
+}
---
+++ ToDoList/Migrations/20231015145924_AddItemTagJoinEntity.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class AddItemTagJoinEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ItemTags",
+                columns: table => new
+                {
+                    ItemTagId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTags", x => x.ItemTagId);
+                    table.ForeignKey(
+                        name: "FK_ItemTags_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "TagId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemTags_ItemId",
+                table: "ItemTags",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemTags_TagId",
+                table: "ItemTags",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "ItemTags");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+    }
+}
